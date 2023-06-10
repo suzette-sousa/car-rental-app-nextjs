a9992ce145cc6604dae7bccfe7806257c989761a
Suzette Sousa <sousa.suzette.pro@gmail.com>
2023-06-10 19:17:55 +0200

    add mea section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
-import Image from 'next/image'
+import Image from "next/image";
+import { Mea } from "@/components";
+
+const testttt = "";
 
 export default function Home() {
   return (
-    <main>
+    <main className="overflow-hidden">
+      <Mea />
     </main>
-  )
+  );
 }
