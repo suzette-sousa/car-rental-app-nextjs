886edcd4923df54950f699f675ac99b2c33ea0cf
Suzette Sousa <sousa.suzette.pro@gmail.com>
2023-06-13 18:03:03 +0200

    fix img gas

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -63,13 +63,7 @@ const CarCard = ({ car }: CarCardProps) => {
           </div>
 
           <div className="flex flex-col justify-center items-center gap-2">
-            <Image
-              src="/gas.svg"
-              width={20}
-              height={20}
-              alt="gas"
-              className="h-[20px] w-auto"
-            />
+            <Image src="/gas.svg" width={20} height={20} alt="gas" />
             <p className="text-[14px]">{city_mpg} l/100km</p>
           </div>
         </div>
