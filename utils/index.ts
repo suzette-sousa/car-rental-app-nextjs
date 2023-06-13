982a25c39769b71ae57cc1988679be3a8efb34ae
Suzette Sousa <sousa.suzette.pro@gmail.com>
2023-06-13 15:13:57 +0200

    complete search with params

--- utils/index.ts
+++ utils/index.ts
@@ -1,13 +1,15 @@
-import { CarProps } from '@/types';
+import { CarProps, FilterProps } from '@/types';
 
-const fetchCars = async () => {
+const fetchCars = async (filters: FilterProps) => {
+  const { manufacturer, year, fuel, limit, model } = filters;
+
   const headers = {
     'X-RapidAPI-Key': process.env.NEXT_PUBLIC_CAR_API_KEY || '',
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
   };
 
   const response = await fetch(
-    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera',
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
     { headers: headers }
   );
 
